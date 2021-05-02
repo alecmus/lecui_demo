c2c694c600374da70866aba2bc005bc95b0b7b00
Alec T. Musasa <alecmus@live.com>
2021-05-02 17:52:39 +0200

    Update lecui_login.cpp

--- lecui_login/lecui_login.cpp
+++ lecui_login/lecui_login.cpp
@@ -1,3 +1,5 @@
+/* Simple login form
+*/
 #include <liblec/lecui/controls.h>
 #include <liblec/lecui/appearance.h>
 #include <liblec/lecui/containers/page.h>
@@ -6,52 +8,53 @@
 #include <liblec/lecui/widgets/password_field.h>
 #include <liblec/lecui/widgets/button.h>
 #include <liblec/lecui/widgets/image_view.h>
-using namespace liblec;
+using namespace liblec::lecui;
+using std::string;
 
-class login : public lecui::form {
+class login : public form {
 	const float margin_ = 10.f;
-	lecui::page_management page_man_{ *this };
-	lecui::controls ctrls_{ *this };
-	lecui::appearance apprnc_{ *this };
-	lecui::dimensions dim_{ *this };
+	page_management page_man_{ *this };
+	controls ctrls_{ *this };
+	appearance apprnc_{ *this };
+	dimensions dim_{ *this };
 	bool& logged_in_;
-	const std::string username_ = "User";
-	const std::string password_ = "password";
+	const string username_ = "User";
+	const string password_ = "password";
 
 public:
-	login(const std::string& caption, bool& logged_in) :
-		lecui::form(caption),
+	login(const string& caption, bool& logged_in) :
+		form(caption),
 		logged_in_(logged_in) {}
 
-	bool on_layout(std::string& error) override {
+	bool on_layout(string& error) override {
 		ctrls_.minimize(false);
 		ctrls_.resize(false);
-		apprnc_.theme(lecui::themes::light);
+		apprnc_.theme(themes::light);
 		dim_.size({ 280, 320 });
 
 		auto& page = page_man_.add("home");
 
-		lecui::widgets::image_view avatar(page, "avatar");
+		widgets::image_view avatar(page, "avatar");
 		avatar().rect.size(130, 130);
 		avatar().rect.place({ margin_, page.size().width - margin_,
 			margin_, margin_ + (dim_.size().height / 2.f) },
 			50.f, 0.f);
-		avatar().quality = lecui::image_quality::high;
+		avatar().quality = image_quality::high;
 		avatar().file = "images\\avatar.png";
 
-		lecui::widgets::text_field username(page, "username");
+		widgets::text_field username(page, "username");
 		username().rect.snap_to(avatar().rect,
-			lecui::rect::snap_type::bottom, 3.f * margin_);
+			rect::snap_type::bottom, 3.f * margin_);
 		username().events().action = [&]() { on_login(); };
 
-		lecui::widgets::password_field password(page, "password");
+		widgets::password_field password(page, "password");
 		password().rect.snap_to(username().rect,
-			lecui::rect::snap_type::bottom, margin_);
+			rect::snap_type::bottom, margin_);
 		password().events().action = [&]() { on_login(); };
 
-		lecui::widgets::button do_login(page);
+		widgets::button do_login(page);
 		do_login().rect.snap_to(password().rect,
-			lecui::rect::snap_type::bottom, 1.5f * margin_);
+			rect::snap_type::bottom, 1.5f * margin_);
 		do_login().text = "Login";
 		do_login().events().click = [this]() { on_login(); };
 
@@ -61,8 +64,8 @@ public:
 
 	void on_login() {
 		try {
-			auto& username = lecui::widgets::text_field::specs(*this, "home/username").text;
-			auto& password = lecui::widgets::text_field::specs(*this, "home/password").text;
+			auto& username = widgets::text_field::specs(*this, "home/username").text;
+			auto& password = widgets::text_field::specs(*this, "home/password").text;
 
 			if (username.empty() || password.empty())
 				return;
@@ -74,14 +77,14 @@ public:
 			else
 				message("Wrong username or password");
 		}
-		catch (const std::exception& e) { message("Error: " + std::string(e.what())); }
+		catch (const std::exception& e) { message("Error: " + string(e.what())); }
 	}
 };
 
 int main() {
 	bool logged_in = false;
 	login login_("Login", logged_in);
-	std::string error;
+	string error;
 	if (login_.show(error)) {
 		if (logged_in)
 			login_.message("Login successful!");
